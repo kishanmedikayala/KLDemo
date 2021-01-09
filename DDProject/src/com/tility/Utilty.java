91682597cc05aaa01622ddca569aa7856d7a0bbf
ajay <ajay@gmail.com>
2021-01-10 00:05:57 +0530

    mmklsmc

--- DDProject/src/com/tility/Utilty.java
+++ DDProject/src/com/tility/Utilty.java
@@ -3,7 +3,7 @@ package com.tility;
 public class Utilty {
 	public void m1()
 	{
-		System.out.println("mlmlmmlsmls,ls;lslsslllll:123333333222222222222");
+		System.out.println("mlmmlsmls,ls;lslsslllll:123333333222222222222");
 	}
 
 }
